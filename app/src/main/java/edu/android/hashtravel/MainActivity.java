74e930fbdd64fcf2f27926622e4c164093eb6a34
jutoungjun <goldrlawndud@naver.com>
2019-05-20 10:26:53 +0900

    수정2

--- app/src/main/java/edu/android/hashtravel/MainActivity.java
+++ app/src/main/java/edu/android/hashtravel/MainActivity.java
@@ -16,6 +16,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.widget.TextView;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -98,6 +100,12 @@ public class MainActivity extends AppCompatActivity
 
             }
         });
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
 
     }
 
