f8a310801811a9150adc2cac6cfe8c25a42f597f
poohhh <ny2322@gmail.com>
2019-05-24 21:03:37 +0900

    Merge branch 'master' of C:\study\lab-android\HashTravel with conflicts.

--- app/src/main/java/edu/android/hashtravel/MainActivity.java
+++ app/src/main/java/edu/android/hashtravel/MainActivity.java
@@ -32,6 +32,8 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener, HomeFragment.ContinentSelectCallback {
@@ -60,7 +62,7 @@ public class MainActivity extends AppCompatActivity
                 case R.id.navigation_dashboard:
                     viewPager.setCurrentItem(1);
                     return true;
-                case R.id.navigation_hotpost:
+                case R.id.navigation_hotplace:
                     viewPager.setCurrentItem(2);
                     return true;
             }
@@ -124,9 +126,10 @@ public class MainActivity extends AppCompatActivity
 
         // ViewPager에 Fragment 추가
         adapter.addFragment(new HomeFragment(), "homefragment");
-        Fragment dashboardFragment = DashboardFragment.getInstance(); // Fragment 생성
+        Fragment dashboardFragment = new DashboardFragment(); // Fragment 생성
         if(mAuth.getCurrentUser() == null){
-            adapter.addFragment(dashboardFragment, "dashboard");
+
+            adapter.addFragment(new DashboardFragment(), "dashboard");
         }else{
             Bundle bundle = new Bundle(1); // 파라미터는 전달할 데이터 개수
             bundle.putString("userId", mAuth.getCurrentUser().getDisplayName()); // key , value
@@ -259,10 +262,10 @@ public class MainActivity extends AppCompatActivity
         if (id == R.id.logInAndOut) {
             logInAndOut();
         } else if (id == R.id.myInfo) {
-            Intent intent = new Intent(this, UserInfo.class);
-            intent.putExtra("email", mAuth.getCurrentUser().getEmail());
-            intent.putExtra("name", mAuth.getCurrentUser().getDisplayName());
-            startActivity(intent);
+          Intent intent = new Intent(this, UserInfo.class);
+          intent.putExtra("email", mAuth.getCurrentUser().getEmail());
+          intent.putExtra("name", mAuth.getCurrentUser().getDisplayName());
+          startActivity(intent);
         } else if (id == R.id.write) {
             // 글쓰기 액티비티로 이동
             Intent intent = new Intent(this, WriteBordActivity.class);
@@ -336,12 +339,20 @@ public class MainActivity extends AppCompatActivity
                         Toast.makeText(MainActivity.this, "User account deleted.", Toast.LENGTH_SHORT).show();
                     }
                 });
-    }
+        }
 
     @Override
-    public void onContinentSelected(String selectContinent) {
-        DashboardFragment dashboardFragment = DashboardFragment.getInstance();
+    public void onContinentSelected(String continent) {
+        DashboardFragment dashboardFragment = new DashboardFragment();
+//        getSupportFragmentManager()
+//                .beginTransaction()
+//                .add(, dashboardFragment)
+//                .commit();
         viewPager.setCurrentItem(1);
-        dashboardFragment.postContinentView(selectContinent);
+
+            Query continentQuery = FirebaseDatabase.getInstance().getReference().child("posts").orderByChild("continent").equalTo(continent);
+
+       dashboardFragment.postContinentView(continentQuery);
     }
 }
+
