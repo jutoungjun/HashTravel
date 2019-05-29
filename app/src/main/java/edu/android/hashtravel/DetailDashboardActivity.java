aea5cc780643906f06a790d98f7d469dd631f59b
poohhh <ny2322@gmail.com>
2019-05-29 16:06:13 +0900

    수정

--- app/src/main/java/edu/android/hashtravel/DetailDashboardActivity.java
+++ app/src/main/java/edu/android/hashtravel/DetailDashboardActivity.java
@@ -9,8 +9,6 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -26,9 +24,6 @@ import com.google.firebase.database.MutableData;
 import com.google.firebase.database.Transaction;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
-
-import java.util.Arrays;
-import java.util.List;
 
 public class DetailDashboardActivity extends AppCompatActivity {
 
